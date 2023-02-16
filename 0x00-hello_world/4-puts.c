0595254c41d0fad21bad764b9a439b0a15047136
marvy896 <onyex896@gmail.com>
2023-02-16 13:25:40 -0800

    Printing using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*main - print the string in the put function
+*Description: using the main function
+*initializing strings
+*assign variables to the string
+*write to the std out
+*return:0
+*/
+
+int main(void)
+{
+char alx[] = "Programming is like building a multilingual puzzle";
+puts(alx);
+return (0);
+}
