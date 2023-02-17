59709956bfdaf4047820b2abeb4c34c85bfad690
marvy896 <onyex896@gmail.com>
2023-02-17 22:39:24 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j = 27;
+	/* your code goes there */
+	while (j < 47)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
