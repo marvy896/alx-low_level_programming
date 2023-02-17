033f96492b886e9264e79ce8940351eb8bffdef0
marvy896 <onyex896@gmail.com>
2023-02-17 01:35:57 +0000

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	exit(1);
+}
