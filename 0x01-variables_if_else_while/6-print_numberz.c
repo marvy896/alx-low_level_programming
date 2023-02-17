8ceaa355a21825981ec262b09a8260602bdb93ef
marvy896 <onyex896@gmail.com>
2023-02-17 22:53:33 +0000

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,11 @@
  */
 int main(void)
 {
-	int j = 0;
 	/* your code goes there */
-	while (j < 9)
-	{
-		putchar(j);
-		j++;
-	}
+	int j;
+
+	for (j = 0; j < 10; j++)
+		putchar(j + '0');
 	putchar('\n');
 	return (0);
 }
