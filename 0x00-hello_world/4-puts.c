74792464871d5145664398f3568beb786d7436c0
marvy896 <onyex896@gmail.com>
2023-02-17 00:44:34 +0000

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
-#include <string.h>
 /**
-*main - print the string in the put function
-*Description: using the main function
-*initializing strings
-*assign variables to the string
-*write to the std out
-*return:0
-*/
+ * * main - print the string in the put function
+ * * Description: using the main function
+ * * this program prints "Programming is like building a multilingual puzzle
+ * * Return: 0
+ * */
 
 int main(void)
 {
-char alx[] = "Programming is like building a multilingual puzzle";
-puts(alx);
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
