5bb89f284585b99ae46cd2d9824feb3110f70a02
marvy896 <onyex896@gmail.com>
2023-02-21 08:15:02 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - print the word _putchar
+ * more headers goes there
+ *
+ *
+ * Return: Always(0)
+ */
+
+int main(void)
+{
+	int a;
+	char b[] = "putchar";
+
+	for (a = 0; a < 9; a++);
+	{
+		_putchar(b[a]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+nested Loops and Functions in C programming
