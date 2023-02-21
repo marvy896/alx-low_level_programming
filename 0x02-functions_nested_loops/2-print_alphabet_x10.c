c67b974e06dd59a78e6e5b81f557ad9cb4ded723
marvy896 <onyex896@gmail.com>
2023-02-21 12:36:47 +0000

    void print_alphabet_x10(void)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10.c - print alphabets in different
+ *
+ * Return: Always 0
+ */
+int void print_alphabet_x10(void)
+{
+	int q, p;
+
+	for (q = 0; q <= 9; q++)
+	{
+		for (p = 27; p <= 122; p++)
+		{
+			_putchar(p);
+		}
+		_putchar('\n')
+	}
+}
