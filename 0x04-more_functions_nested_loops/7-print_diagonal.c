1302de47bd09b321c13181aa7b20c9e8e07cdb87
marvy896 <onyex896@gmail.com>
2023-02-23 19:09:14 +0000

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,13 +1,27 @@
 #include "main.h"
 /**
- * print_line - Write a function that draws a straight line in the terminal.
- * @n: Integer amount of _
+ * print_diagonal - draws a diagonal line
+ * @n: takes in an integer
  */
-void print_line(int n)
+void print_diagonal(int n)
 {
-	char c;
+	int i, j;
 
-	for (c = 0; c < n; c++)
-		_putchar('_');
-	_putchar('\n');
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (i == j)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	}
 }
