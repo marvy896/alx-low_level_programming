31554bfa0feca0a0149570d1ce13d793c5be72de
marvy896 <onyex896@gmail.com>
2023-02-27 16:38:35 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, followed by a new line
+ * @s: pointer to the string to print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+	int index;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	index = i - 1;
+	for (; index >= 0; index--)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
+}
