49fe4357abf195914fc98cb644551212ef145251
Marvy896 <onyex896@gmail.com>
2023-03-08 18:46:20 +0000

    e a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,16 +11,16 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '\0')
-		return (0);
 	if (*s2 == '*')
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	if (*s1 == '\0')
 	{
-		if (*s2 != '*')
-			return (0);
-		else
+		if (*(s2 + 1) == '*')
+		{
 			return (wildcmp(s1, s2 + 1));
+		}
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+		{
+			return (1);
+		}
 	}
 	return (0);
 }
