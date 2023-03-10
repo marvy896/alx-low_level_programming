6889767576ff8b46aba5026a963e8bacc3198a55
Marvy896 <onyex896@gmail.com>
2023-03-10 15:18:43 +0000

    Create the static library libmy.a containing all the functions listed below

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,4 +1,9 @@
 #include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
