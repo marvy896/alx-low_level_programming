5aeb786223c27f27f46eccd03ad9c92dd910194f
Marvy896 <onyex896@gmail.com>
2023-03-10 15:04:51 +0000

    Create the static library libmy.a containing all the functions listed below:

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
  * _puts - prints a string, followed by a new line,
  * @str: pointer to the string to print
@@ -6,11 +10,5 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
+	puts(str);
 }
