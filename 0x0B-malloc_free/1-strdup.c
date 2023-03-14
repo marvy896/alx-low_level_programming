67c5959282aa9ad1d88a24a1ce6d40831e2043bf
Marvy896 <onyex896@gmail.com>
2023-03-14 08:56:41 +0000

    Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string
+ * Return: pointer of an array of chars
+ */
+char *_strdup(char *str)
+{
+	char *sti;
+	unsigned int a, b;
+
+	if (str == NULL)
+		return (NULL);
+	for (a = 0; str[a] != '\0'; a++)
+		;
+	sti = (char *)malloc(sizeof(char) * (a + 1));
+	if (sti == NULL)
+		return (NULL);
+	for (b = 0; b <= a; b++)
+		sti[b] = str[j];
+	return (sti);
+}
